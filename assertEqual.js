4611535a3746d1bb7a4c676c6a9847c8a9a07c72
Calvin Chung <calvin-chung@live.com>
2022-03-07 19:29:15 -0800

    assertion

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("string", "string");
+assertEqual("stringy", "string");
+assertEqual(4, 4);
+assertEqual(4, "four");
