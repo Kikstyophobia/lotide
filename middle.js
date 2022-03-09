512eb3a43e4e19011788ceae1726357e2e7afa28
Calvin Chung <calvin-chung@live.com>
2022-03-09 15:12:57 -0800

    middle arrays ez

---
+++ middle.js
@@ -0,0 +1,48 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  } else {
+  for (let i = 0; i < arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  let isEqual = eqArrays(firstArray, secondArray) 
+  if (isEqual) {
+    console.log("✅ Assertion Passed!");
+  } else {
+    console.log("❌ Assertion Failed!");
+  }
+};
+
+const middle = function(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    // even arrays
+    if (array.length > 2 && array.length % 2 === 0) {
+      newArray.push(array.length/2);
+      newArray.push(array.length/2 + 1);
+      return newArray;
+    // odd arrays
+    } else if (array.length > 2 && array.length % 2 !== 0) {
+      newArray.push(Math.round(array.length/2));
+      return newArray;
+    }
+    // non-applicable arrays
+    else {
+      return [];
+    }
+  }
+};
+
+assertArraysEqual(middle([1]), []) // => []
+assertArraysEqual(middle([1, 2]), []) // => []
+assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2,3]) // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]) // => [3, 4]
