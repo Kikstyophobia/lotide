cd88a06c43a3e0ff8b192c6c3a9838d617b0e983
Calvin Chung <calvin-chung@live.com>
2022-03-08 22:42:47 -0800

    without

--- without.js
+++ without.js
@@ -47,3 +47,4 @@ const words = ["hello", "world", "lighthouse"];
 console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
