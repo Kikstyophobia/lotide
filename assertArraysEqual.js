dc23a2283ca86a0eb220beb7785014dad2535f37
Calvin Chung <calvin-chung@live.com>
2022-03-14 14:11:55 -0700

    assertArraysEquals updated with module.exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = function(arrOne, arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  } else {
-  for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] !== arrTwo[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 
 const assertArraysEqual = function(firstArray, secondArray) {
